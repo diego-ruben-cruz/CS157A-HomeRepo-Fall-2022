fe47fd532684e179063b96814e2fc3089abd02b1
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2022-11-09 16:22:45 -0800

    Began work on proj1

--- hw4-First-JDBC/src/BankingSystem.java
+++ hw4-First-JDBC/src/BankingSystem.java
@@ -1,4 +1,4 @@
-package src;
+// package src;
 
 import java.io.FileInputStream;
 import java.sql.Connection;
@@ -16,7 +16,7 @@ public class BankingSystem {
 	private static String url;
 	private static String username;
 	private static String password;
-	
+
 	// JDBC Objects
 	private static Connection con;
 	private static Statement stmt;
@@ -24,22 +24,24 @@ public class BankingSystem {
 
 	/**
 	 * Initialize database connection given properties file.
+	 * 
 	 * @param filename name of properties file
 	 */
 	public static void init(String filename) {
 		try {
-			Properties props = new Properties();						// Create a new Properties object
-			FileInputStream input = new FileInputStream(filename);	// Create a new FileInputStream object using our filename parameter
-			props.load(input);										// Load the file contents into the Properties object
-			driver = props.getProperty("jdbc.driver");				// Load the driver
-			url = props.getProperty("jdbc.url");						// Load the url
-			username = props.getProperty("jdbc.username");			// Load the username
-			password = props.getProperty("jdbc.password");			// Load the password
+			Properties props = new Properties(); // Create a new Properties object
+			FileInputStream input = new FileInputStream(filename); // Create a new FileInputStream object using our
+																	// filename parameter
+			props.load(input); // Load the file contents into the Properties object
+			driver = props.getProperty("jdbc.driver"); // Load the driver
+			url = props.getProperty("jdbc.url"); // Load the url
+			username = props.getProperty("jdbc.username"); // Load the username
+			password = props.getProperty("jdbc.password"); // Load the password
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Test database connection.
 	 */
@@ -50,117 +52,182 @@ public class BankingSystem {
 			con = DriverManager.getConnection(url, username, password);
 			con.close();
 			System.out.println(":: TEST - SUCCESSFULLY CONNECTED TO DATABASE");
-			} catch (Exception e) {
-				System.out.println(":: TEST - FAILED CONNECTED TO DATABASE");
-				e.printStackTrace();
-			}
-	  }
+		} catch (Exception e) {
+			System.out.println(":: TEST - FAILED CONNECTED TO DATABASE");
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Create a new customer.
-	 * @param name customer name
+	 * 
+	 * @param name   customer name
 	 * @param gender customer gender
-	 * @param age customer age
-	 * @param pin customer pin
+	 * @param age    customer age
+	 * @param pin    customer pin
 	 */
-	public static void newCustomer(String name, String gender, String age, String pin) 
-	{
+	public static void newCustomer(String name, String gender, String age, String pin) {
 		System.out.println(":: CREATE NEW CUSTOMER - RUNNING");
-				/* insert your code here */
-		System.out.println(":: CREATE NEW CUSTOMER - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery("INSERT INTO p1.customer(name, gender, age, pin) VALUES (" + name + ", " + gender
+					+ ", " + age + ", " + pin + ");");
+			System.out.println(":: CREATE NEW CUSTOMER - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: CREATE NEW CUSTOMER - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Open a new account.
-	 * @param id customer id
-	 * @param type type of account
+	 * 
+	 * @param id     customer id
+	 * @param type   type of account
 	 * @param amount initial deposit amount
 	 */
-	public static void openAccount(String id, String type, String amount) 
-	{
+	public static void openAccount(String id, String type, String amount) {
 		System.out.println(":: OPEN ACCOUNT - RUNNING");
-				/* insert your code here */
-		System.out.println(":: OPEN ACCOUNT - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery("INSERT INTO p1.account(id, type, balance) VALUES (" + id + ", " + type
+					+ ", " + amount + ");");
+			System.out.println(":: OPEN ACCOUNT - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: OPEN ACCOUNT - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Close an account.
+	 * 
 	 * @param accNum account number
 	 */
-	public static void closeAccount(String accNum) 
-	{
+	public static void closeAccount(String accNum) {
 		System.out.println(":: CLOSE ACCOUNT - RUNNING");
-				/* insert your code here */
-		System.out.println(":: CLOSE ACCOUNT - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery("DELETE FROM p1.account WHERE p1.account.number = " + accNum + ";");
+			System.out.println(":: CLOSE ACCOUNT - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: CLOSE ACCOUNT - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Deposit into an account.
+	 * 
 	 * @param accNum account number
 	 * @param amount deposit amount
 	 */
-	public static void deposit(String accNum, String amount) 
-	{
+	public static void deposit(String accNum, String amount) {
 		System.out.println(":: DEPOSIT - RUNNING");
-				/* insert your code here */
-		System.out.println(":: OPEN ACCOUNT - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery(
+					"UPDATE p1.account SET balance = balance + " + amount + " WHERE number = " + accNum + ";");
+			System.out.println(":: DEPOSIT - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: DEPOSIT - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Withdraw from an account.
+	 * 
 	 * @param accNum account number
 	 * @param amount withdraw amount
 	 */
-	public static void withdraw(String accNum, String amount) 
-	{
+	public static void withdraw(String accNum, String amount) {
 		System.out.println(":: WITHDRAW - RUNNING");
-				/* insert your code here */
-		System.out.println(":: WITHDRAW - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			stmt.executeQuery(
+					"UPDATE p1.account SET balance = balance - " + amount + " WHERE number = " + accNum + ";");
+			System.out.println(":: WITHDRAW - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: WITHDRAW - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Transfer amount from source account to destination account. 
-	 * @param srcAccNum source account number
+	 * Transfer amount from source account to destination account.
+	 * 
+	 * @param srcAccNum  source account number
 	 * @param destAccNum destination account number
-	 * @param amount transfer amount
+	 * @param amount     transfer amount
 	 */
-	public static void transfer(String srcAccNum, String destAccNum, String amount) 
-	{
+	public static void transfer(String srcAccNum, String destAccNum, String amount) {
 		System.out.println(":: TRANSFER - RUNNING");
-				/* insert your code here */	
-		System.out.println(":: TRANSFER - SUCCESS");
+		try {
+			BankingSystem.withdraw(srcAccNum, amount);
+			BankingSystem.deposit(destAccNum, amount);
+			System.out.println(":: TRANSFER - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: TRANSFER - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Display account summary.
+	 * 
 	 * @param cusID customer ID
 	 */
-	public static void accountSummary(String cusID) 
-	{
+	public static void accountSummary(String cusID) {
 		System.out.println(":: ACCOUNT SUMMARY - RUNNING");
-				/* insert your code here */		
-		System.out.println(":: ACCOUNT SUMMARY - SUCCESS");
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT number, balance FROM p1.account WHERE id = " + cusID + ";");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT SUM(balance) AS total FROM p1.account WHERE id = " + cusID + ";");
+			System.out.println(":: ACCOUNT SUMMARY - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: ACCOUNT SUMMARY - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Display Report A - Customer Information with Total Balance in Decreasing Order.
+	 * Display Report A - Customer Information with Total Balance in Decreasing
+	 * Order.
 	 */
-	public static void reportA() 
-	{
-		System.out.println(":: REPORT A - RUNNING");
-				/* insert your code here */	
-		System.out.println(":: REPORT A - SUCCESS");
+	public static void reportA() {
+		System.out.println(":: DISPLAY REPORT A - RUNNING");
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(
+					"SELECT id, name, gender, age, SUM(balance) AS TOTAL FROM p1.customer JOIN p1.account ON p1.customer.id = p1.account.id GROUP BY id ORDER BY TOTAL DESC;");
+			System.out.println(":: ACCOUNT SUMMARY - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: ACCOUNT SUMMARY - FAILED");
+			e.printStackTrace();
+		}
 	}
 
 	/**
-	 * Display Report B - Customer Information with Total Balance in Decreasing Order.
+	 * Display Report B - Average Total Balance within an age range
+	 * 
 	 * @param min minimum age
 	 * @param max maximum age
 	 */
-	public static void reportB(String min, String max) 
-	{
+	public static void reportB(String min, String max) {
 		System.out.println(":: REPORT B - RUNNING");
-				/* insert your code here */		
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(
+					"SELECT AVG(balance) AS TOTAL FROM p1.customer JOIN p1.account ON p1.customer.id = p1.account.id WHERE p2.customer.age >= "
+							+ min + " AND p1.customer.age <= " + max + ";");
+			System.out.println(":: REPORT B - SUCCESS");
+		} catch (Exception e) {
+			System.out.println(":: REPORT B - FAILED");
+			e.printStackTrace();
+		}
+
 		System.out.println(":: REPORT B - SUCCESS");
 	}
 }
